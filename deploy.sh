a5c1cbe799d196a0e55894109582ca03c4afc6c1
kazuhito_m <kazuhito.sumpic@gmail.com>
2020-01-16 00:12:23 +0900

    implement: デプロイ用のスクリプトを追加。
    まだ、デバッグ中…。

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REGION=asia-northeast1
+
+# kickしたディレクトリから、このスクリプトのあるディレクトリに移動。
+cd $(cd $(dirname $0);pwd)
+
+gcloud beta functions deploy slackChoicesBot \
+    --runtime nodejs8 \
+    --trigger-http \
+    --region asia-northeast1

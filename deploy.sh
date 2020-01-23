dbdf1e3bfc5ff51233d8111be9dda8fa4cff008b
kazuhito_m <kazuhito.sumpic@gmail.com>
2020-01-23 23:42:25 +0900

    refactor: せっかく設定変数をつくっていたのに使ってなかった。修正。

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REGION=asia-northeast2
+REGION=asia-northeast1
 
 # kickしたディレクトリから、このスクリプトのあるディレクトリに移動。
 cd $(cd $(dirname $0);pwd)
@@ -8,4 +8,4 @@ cd $(cd $(dirname $0);pwd)
 gcloud beta functions deploy slackChoicesBot \
     --runtime nodejs8 \
     --trigger-http \
-    --region asia-northeast1
+    --region ${REGION}

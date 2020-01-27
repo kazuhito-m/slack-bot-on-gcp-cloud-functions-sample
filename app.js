53ed7daf23db3ed1503ec9b06d2b1475414b1304
kazuhito_m <kazuhito.sumpic@gmail.com>
2020-01-27 23:02:45 +0900

    implement: 一通り「カタチとして見せられるもの」まで、Slack部分を作成。
    ただ、まだ適当なのと、GCP部分が無いので、ここまで。

--- app.js
+++ app.js
@@ -5,17 +5,16 @@ async function postMessage(payload) {
         headers: { 'Authorization': `Bearer ${process.env.BOT_USER_TOKEN}` },
         json: payload,
     });
-    if (!res.ok) {
-        console.error(res);
-    }
+    if (!res.ok) console.error('postMessage()にてPostする箇所にてエラー。res:' + JSON.stringify(res));
     return res;
 }
 
 function createSlackAttachment(message) {
+    console.log('createSlackAttachment()まで来ました:' + message);
     const choices = [
-        { text: 'A', value: 'value-A' },
-        { text: 'B', value: 'value-B' },
-        { text: 'C', value: 'value-C' },
+        { text: '(キャンセル)', value: 'value:__cancel__' },
+        { text: '0.0.19', value: 'value:0.0.19' },
+        { text: '0.0.18', value: 'value:0.0.18' },
     ];
     return {
         text: message,
@@ -24,7 +23,7 @@ function createSlackAttachment(message) {
         callback_id: 'env_selection',
         actions: [{
             name: 'choices',
-            text: 'Please select!',
+            text: 'Version(Container Tag)を選択してください。',
             type: 'select',
             options: choices,
         }],
@@ -33,20 +32,22 @@ function createSlackAttachment(message) {
 
 const onRequest = async (req, res) => {
     let payload = req.body;
-    console.log(payload);
+    console.log('onRequest()まで来ました。PayLoad:' + payload);
 
     if (payload.type === 'url_verification') {
-        return res.status(200).json({ 'challenge': payload.challenge });
+        return res.status(200)
+            .json({ 'challenge': payload.challenge });
     }
 
     if (payload.event && payload.event.type === 'app_mention') {
         if (payload.event.text.includes('hi')) {
             const slackRes = await postMessage({
-                text: `<@${payload.event.user}> hi!`,
+                text: `やあ! <@${payload.event.user}> さん。`,
                 channel: payload.event.channel,
-                attachments: [createSlackAttachment('BOT response')],
+                attachments: [createSlackAttachment('Version(Container Tag)を選択してください。')],
             });
-            return res.status(200).json(slackRes);
+            return res.status(200)
+                .json(slackRes);
         }
     }
 
@@ -58,11 +59,52 @@ const onRequest = async (req, res) => {
         const action = payload.actions[0];
         if (action.name === 'choices') {
             const selectedOption = action.selected_options[0];
-            return res.status(200).send(`<@${payload.user.id}> Your select value: "${selectedOption.value}"`);
+            const o = {
+                "text": `<@${payload.user.id}>  環境:production に Version(Tag):"${selectedOption.value}" でデプロイを行います。`,
+                "attachments": [
+                    {
+                        "text": "デプロイして良いですか？",
+                        "fallback": "You are unable to choose a game",
+                        "callback_id": "do_deploy",
+                        "color": "danger",
+                        "attachment_type": "default",
+                        "actions": [
+                            {
+                                "name": "deploy",
+                                "text": "Yes!",
+                                "type": "button",
+                                "value": `result:true, environment: production, version:${selectedOption.value}`
+                            },
+                            {
+                                "name": "deploy",
+                                "text": "No.",
+                                "type": "button",
+                                "value": "result:false"
+                            }
+                        ]
+                    }
+                ]
+            }
+            return res.status(200)
+                .json(o);
         }
     }
 
-    return res.status(200).send('OK');
+    if (payload && payload.type === 'interactive_message') {
+        const action = payload.actions[0];
+        if (action.name === 'deploy') {
+            if (!action.value.startsWith('result:true')) {
+                return res.status(200)
+                    .send('デプロイを取りやめました。');
+            }
+            const caption =  action.value.replace(/.*true, /, '');
+            const description = `${caption} で、デプロイ依頼を受け付けました。\n結果は各種環境のチャンネルでご確認ください。\n <#hsi-env-prd>`;
+            return res.status(200)
+                .send(description);
+        }
+    }
+
+    return res.status(200).send('はい、なんでしょうか？');
 };
 
 exports.slackChoicesBot = onRequest;
